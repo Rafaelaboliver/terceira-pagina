4a4f25dc2067ceaa56d6c4c425e44e25bb6c6689
rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2022-10-16 18:47:42 -0300

    html/css/javasript desenvolvidos ate a parte que o usuario escolhe os tres itens e libera o botao para finalizar o pedido

--- script.js
+++ script.js
@@ -1,51 +1,73 @@
 //para mostrar qual prato foi selecionado
 
-const foodOne = document.querySelector(".menu-option1");
-const foodTwo = document.querySelector(".menu-option2");
-const foodThree = document.querySelector(".menu-option3");
+let mainFinal;
 
-function choiceFoodOne (){
-    foodOne.classList.toggle('border-check');
+function choiceFood (food){
+    //verificar se existe um botão que tenha sido selecionado anteriormente
+    const foodSelection = document.querySelector('.main-option .border-check');
+
+    //se existir
+    if (foodSelection !== null) {
+        //remover border que está atribuída à classe selecionada anteriormente
+       foodSelection.classList.remove('border-check')
+    }
+        
+    //adicionar border à classe selecionada
+    food.classList.add('border-check');
+
+    mainFinal = food.innerHTML;
+    allDone ();
 }
 
-function choiceFoodTwo (){
-    foodTwo.classList.toggle('border-check');
+
+let drinkFinal;
+
+function choiceDrink (drink){
+    
+    const drinkSelection = document.querySelector('.drink-option .border-check');
+
+    if (drinkSelection !== null){
+        drinkSelection.classList.remove('border-check');
+    }
+ 
+    drink.classList.add('border-check');
+
+    drinkFinal = drink.innerHTML;
+    allDone ();
 }
 
-function choiceFoodThree (){
-    foodThree.classList.toggle('border-check');
+
+let dessertFinal;
+
+function choiceDessert (dessert){
+    const dessertSelection = document.querySelector('.dessert-option .border-check');
+
+    if (dessertSelection !== null) {
+        dessertSelection.classList.remove('border-check');
+    }
+   
+    dessert.classList.add('border-check');
+
+    dessertFinal = dessert.innerHTML;
+    allDone ();
 }
 
+//liberar para APARECER o botão de fechar pedido
+    //criar função para saber se cada categoria do pedido -food/dronk/dessert- foi selecionado
+    function allDone (){
+        if(mainFinal !== undefined){
+            if(drinkFinal !== undefined){
+                if (dessertFinal !== undefined){
+                    const threeItems = document.querySelector('.message');
+                    threeItems.classList.add('hide')
+                    
+                    const finish = document.querySelector('.finish-order');
+                    finish.classList.remove('hide');
+                }
+            }
+        }
+    }
 
-const drinkOne = document.querySelector(".menu-option4");
-const drinkTwo = document.querySelector(".menu-option5");
-const drinkThree = document.querySelector(".menu-option6");
 
-function choiceDrinkOne (){
-    drinkOne.classList.toggle('border-check');
-}
 
-function choiceDrinkTwo (){
-    drinkTwo.classList.toggle('border-check');
-}
-
-function choiceDrinkThree (){
-    drinkThree.classList.toggle('border-check');
-}
-
-const dessertOne = document.querySelector(".menu-option7")
-const dessertTwo = document.querySelector(".menu-option8");
-const dessertThree = document.querySelector(".menu-option9");
-
-function choiceDessertOne (){
-    dessertOne.classList.toggle('border-check');
-}
-
-function choiceDessertTwo (){
-    dessertTwo.classList.toggle('border-check');
-}
-
-function choiceDessertThree (){
-    dessertThree.classList.toggle('border-check');
-}
-//condição para selecionar apenas um ítem
+ //função para EXECUTAR o botão de fechar pedido - encaminhar para o whatsapp web
